45375cc646f62ade47a774e287dc993c39219c55
Hunt, David <davidhunt@mitre.org>
2020-02-26 16:12:16 -0500

    fixing a flag

--- app/flags/adversaries/flag_1.py
+++ app/flags/adversaries/flag_1.py
@@ -3,8 +3,9 @@ challenge = 'Add the Nosy Neighbor adversary pack to the Super Spy adversary and
 
 
 async def verify(services):
-    for a in await services.get('data_svc').locate('adversaries', dict(ability_id='564ae20d-778d-4965-93dc-b523be2e2ab4')):
-        for _, ability in a.items():
-            if ability.ability_id == '2fe2d5e6-7b06-4fc0-bf71-6966a1226731':
-                return True
+    for a in await services.get('data_svc').locate('adversaries', dict(adversary_id='564ae20d-778d-4965-93dc-b523be2e2ab4')):
+        for _, abilities in a.phases.items():
+            for ability in abilities:
+                if ability.ability_id == '2fe2d5e6-7b06-4fc0-bf71-6966a1226731':
+                    return True
     return False
