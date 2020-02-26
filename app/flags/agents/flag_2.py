30e315fd8083df84c836c8127737c7ae3e7c0356
Hunt, David <davidhunt@mitre.org>
2020-02-26 16:01:45 -0500

    adding verifications for agent flags

--- app/flags/agents/flag_2.py
+++ app/flags/agents/flag_2.py
@@ -3,4 +3,6 @@ challenge = 'Change the untrusted agent timer to 90 seconds. Then ensure one of 
 
 
 async def verify(services):
+    if services.get('contact_svc').untrusted_timer == 90:
+        return True
     return False
--- app/flags/agents/flag_5.py
+++ app/flags/agents/flag_5.py
@@ -1,6 +1,6 @@
 name = 'Bootstrap abilities'
-challenge = 'Ensure that new agents automatically run "whoami" on their first beacon'
+challenge = 'Using a Stockpile ability, ensure new agents automatically run "whoami" on their first beacon'
 
 
 async def verify(services):
-    return False
+    return 'c0da588f-79f0-4263-8998-7496b1a40596' in services.get('contact_svc').bootstrap_instructions
