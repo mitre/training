434c74e93edbb3d253330919b861ada98157768d
Hunt, David <davidhunt@mitre.org>
2020-03-18 16:02:37 -0500

    adding a few more tweaks to how the cert is shown on the ui

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
+from base64 import b32encode
+
 from plugins.training.app.c_flag import Flag
 
 
 def calculate_code(self):
     if self.started_ts and self.completed_ts:
-        return '%d+%d+%d' % (self.number, abs((self.completed_ts - self.started_ts).seconds), self._ticks)
-    return '%d+0+%d' % (self.number, self._ticks)
+        return str(b32encode('%d+%d+%d'.encode() % (self.number, abs((self.completed_ts - self.started_ts).seconds), self._ticks)), 'utf-8')
+    return str(b32encode('%d+0+%d'.encode() % (self.number, self._ticks)), 'utf-8')
 
 
 Flag.calculate_code = calculate_code
