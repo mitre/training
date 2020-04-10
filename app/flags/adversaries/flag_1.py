c925d4aff185637b4cfccaa9dd8174517b2845c7
Alex <43142871+khyberspache@users.noreply.github.com>
2020-04-10 11:52:04 -0400

    patching flags for new adversary file structure (#52)

--- app/flags/adversaries/flag_1.py
+++ app/flags/adversaries/flag_1.py
@@ -1,14 +1,13 @@
-name = 'Add a pack'
-challenge = 'Add the Nosy Neighbor adversary pack to the Certifiable adversary and save it'
+name = 'Combine Adversary Profiles'
+challenge = 'Add the Nosy Neighbor adversary to the Certifiable adversary and save it'
 extra_info = """A TTP is often connected to a series of other TTPs to form an attack. For example, an adversary may have a procedure
 for locating all PDF files on a host. This may be followed by a procedure to exfiltrate all the found files. In
-CALDERA, this chain of TTPs being used is considered a pack."""
+CALDERA, this chain of TTPs being used is considered an adversary."""
 
 
 async def verify(services):
     for a in await services.get('data_svc').locate('adversaries', dict(name='Certifiable')):
-        for _, abilities in a.phases.items():
-            for ability in abilities:
-                if ability.ability_id == '2fe2d5e6-7b06-4fc0-bf71-6966a1226731':
-                    return True
+        for ability in a.atomic_ordering:
+            if ability.ability_id == '2fe2d5e6-7b06-4fc0-bf71-6966a1226731':
+                return True
     return False
--- app/flags/developers/flag_1.py
+++ app/flags/developers/flag_1.py
@@ -8,9 +8,9 @@ extra_info = ''
 async def verify(services):
     for op in await services.get('data_svc').locate('operations'):
         if op.finish and op.planner.name == 'think_hard' and len(op.chain) >= 10:
-            alphabetical_phase = []
-            for phase, abilities in op.adversary.phases.items():
+            alphabetical_op = []
+            for abilities in op.adversary.atomic_ordering:
                 ability_names = [link.ability.name for link in op.chain if link.ability.ability_id in [a.ability_id for a in abilities]]
-                alphabetical_phase.append(all(ability_names[i] <= ability_names[i + 1] for i in range(len(ability_names) - 1)))
-            return all(alphabetical_phase)
+                alphabetical_op.append(all(ability_names[i] <= ability_names[i + 1] for i in range(len(ability_names) - 1)))
+            return all(alphabetical_op)
     return False
