77a5f97911a75e18533ad941119700880e3fd51f
garunagiri <garunagiri@mitre.org>
2020-05-07 17:10:28 -0400

    I thought I had pushed the right thing

--- app/flags/plugins/gameboard/flag_0.py
+++ app/flags/plugins/gameboard/flag_0.py
@@ -4,10 +4,11 @@ from app.utility.base_world import BaseWorld
 
 name = 'GameBoard plugin'
 challenge = 'Run a red-blue exercise. Create a Red operation named exactly \'Gameboard - Red\', and then run a Blue ' \
-            'operation against it named exactly \'Gameboard - Blue\'.'
-extra_info = """Purple-teaming is the process of running a red-team engagement in conjunction with the blue-team. It 
-is often fruitful to provide some - but not all - of the details between the red and blue teams. This allows both sides 
-to train against each other."""
+            'operation against it named exactly \'Gameboard - Blue\'. The results of this exercise can be seen in ' \
+            'the Gameboard plugin modal.'
+extra_info = """Purple-teaming is the process of running a red-team engagement in conjunction with the blue-team. It
+ is often fruitful to provide some - but not all - of the details between the red and blue teams. This allows both sides
+ to train against each other."""
 
 
 async def verify(services):
