c505894b96b32ffc23e31a1545e5e88aa3ad6ad8
garunagiri <garunagiri@mitre.org>
2020-05-08 16:29:35 -0400

    flag works correctly now

--- app/flags/att&ck/blue_0.py
+++ app/flags/att&ck/blue_0.py
@@ -8,18 +8,18 @@ extra_info = """whoami"""
 
 async def verify(services):
     adversaries = await services.get('data_svc').locate('adversaries', match=dict(name='blue_quiz_1'))
-    technique = 'System Owner/User Discovery'
+    technique = 'T1033'  # System Owner User Discovery
     for adv in adversaries:
-        match = await does_technique_match(services.get('data_svc', adv, technique))
-        services.get('rest_svc').delete_adversary(adv.adversary_id)
+        match = await does_technique_match(services, adv, technique)
+        await services.get('rest_svc').delete_adversary(dict(adversary_id=adv.adversary_id))
         if match:
             return True
     return False
 
 
-async def does_technique_match(data_svc, adv, technique):
+async def does_technique_match(services, adv, technique):
     techniques = set()
     for ab_id in adv.atomic_ordering:
-        for abilities in await data_svc.locate('abilities', match=dict(ability_id=ab_id)):
-            techniques.update([ab.technique_id for ab in abilities])
-    return technique in techniques
+        techniques.add((await services.get('rest_svc').display_objects('abilities',
+                                                                       data=dict(ability_id=ab_id)))[0]['technique_id'])
+    return technique in techniques and len(techniques) == 1
