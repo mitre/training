d98a3cbac9a3a98734e9a67a2f6da7eddeb43fd0
garunagiri <garunagiri@mitre.org>
2020-05-11 16:12:11 -0400

    redoing, att&ck badge renamed to attack

---
+++ app/flags/attack/blue_2.py
@@ -0,0 +1,25 @@
+name = 'ATT&CK Quiz 3'
+challenge = 'Refer to \'ATT&CK Quiz 1\' for instructions. Name the uploaded adversary exactly \'blue_quiz_3\'. ' \
+            'Ensure that you\'re starting from a fresh layer on the Compass plugin.\n\n' \
+            'The adversary procedure is:\nnet /y use \\#{remote.host.name} &' \
+            '\ncopy /y sandcat.go-windows\n\\\\#{remote.host.name}\\Users\\Public'
+extra_info = """"""
+
+
+async def verify(services):
+    adversaries = await services.get('data_svc').locate('adversaries', match=dict(name='blue_quiz_3'))
+    technique = 'T1105'  # Remote File Copy
+    for adv in adversaries:
+        match = await does_technique_match(services, adv, technique)
+        await services.get('rest_svc').delete_adversary(dict(adversary_id=adv.adversary_id))
+        if match:
+            return True
+    return False
+
+
+async def does_technique_match(services, adv, technique):
+    techniques = set()
+    for ab_id in adv.atomic_ordering:
+        techniques.add((await services.get('rest_svc').display_objects('abilities',
+                                                                       data=dict(ability_id=ab_id)))[0]['technique_id'])
+    return technique in techniques and len(techniques) == 1
