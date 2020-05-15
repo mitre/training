2ed65cc960126f65ab312be3f831d309557fc7de
Mitch Kucia <mkucia@mitre.org>
2020-05-15 10:54:14 -0400

    Added chain length check to 'Base64 Operation', removed from 'Basic Operation'

--- app/flags/operations/flag_0.py
+++ app/flags/operations/flag_0.py
@@ -7,6 +7,6 @@ emulation exercises, ensuring that each one is identical to the last."""
 
 async def verify(services):
     for op in await services.get('data_svc').locate('operations'):
-        if op.finish and len(op.chain) > 5 and op.adversary.adversary_id == 'de07f52d-9928-4071-9142-cb1d3bd851e8':
+        if op.finish and op.adversary.adversary_id == 'de07f52d-9928-4071-9142-cb1d3bd851e8':
             return True
     return False
