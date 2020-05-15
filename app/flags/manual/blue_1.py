b9736ed7a049a7127215a9859adc90d7f6d02be4
garunagiri <garunagiri@mitre.org>
2020-05-15 16:57:12 -0400

    red parts of flags 1&2 are now started automatically

--- app/flags/manual/blue_1.py
+++ app/flags/manual/blue_1.py
@@ -1,3 +1,4 @@
+from plugins.training.app.base_flag import BaseFlag
 from app.utility.base_world import BaseWorld
 
 
@@ -6,8 +7,22 @@ challenge = 'Start a listening process on Port 7011 on the Linux or Darwin machi
             'ability in the \'Blue Manual\' operation to detect this process.'
 extra_info = """"""
 
+operation_name = 'training_manual_1'
+adversary_id = '6e19d5f9-e4d9-42b2-bc89-ea4298541d80'
+agent_group = 'cert-nix'
+
 
 async def verify(services):
+    if await BaseFlag.does_agent_exist(services, agent_group):
+        if not (await BaseFlag.is_operation_started(services, operation_name)):
+            await BaseFlag.start_operation(services, operation_name, agent_group, adversary_id)
+        if await BaseFlag.is_operation_successful(services, operation_name) and await is_flag_satisfied(services):
+            await BaseFlag.cleanup_operation(services, operation_name)
+            return True
+    return False
+
+
+async def is_flag_satisfied(services):
     for op in await services.get('data_svc').locate('operations',
                                                     match=dict(access=BaseWorld.Access.BLUE, name='Blue Manual')):
         if is_unauth_process_detected(op):
