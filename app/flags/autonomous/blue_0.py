cc9669964a5adba3c3cb9fcd7815643f5695852f
Daniel Kim <kimd@mitre.org>
2020-05-15 12:51:34 -0400

    Renaming sequential planner to batch

--- app/flags/autonomous/blue_0.py
+++ app/flags/autonomous/blue_0.py
@@ -3,7 +3,7 @@ from app.utility.base_world import BaseWorld
 
 name = 'Enable Autonomous Operation'
 challenge = 'Start an autonomous blue operation using the Incident Responder defender profile. Ensure that the ' \
-            'sequential planner is used. Name this operation exactly \'Blue Autonomous\'. Keep this operation open ' \
+            'batch planner is used. Name this operation exactly \'Blue Autonomous\'. Keep this operation open ' \
             'to successfully complete the flags for this badge.'
 extra_info = """"""
 
@@ -12,6 +12,6 @@ async def verify(services):
     for op in await services.get('data_svc').locate('operations',
                                                     match=dict(access=BaseWorld.Access.BLUE, name='Blue Autonomous')):
         if op.adversary.adversary_id == '7e422753-ad7a-4401-bc8b-b12a28e69c25' and not op.atomic \
-                and op.planner.name == 'sequential':
+                and op.planner.name == 'batch':
             return True
     return False
