27e8fd8d5bad74bf774071cbf030fac9579ba82f
garunagiri <garunagiri@mitre.org>
2020-05-15 15:49:22 -0400

    base_flag.py to hold commonly used functions

---
+++ app/base_flag.py
@@ -0,0 +1,33 @@
+from app.utility.base_world import BaseWorld
+
+
+class BaseFlag:
+    """
+    A collection of static functions to be used by training flags.
+    """
+
+    @staticmethod
+    async def does_agent_exist(services, group):
+        return len(await services.get('data_svc').locate('agents', match=dict(group=group)))
+
+    @staticmethod
+    async def is_operation_started(services, op_name):
+        return len(await services.get('data_svc').locate('operations', match=dict(name=op_name)))
+
+    @staticmethod
+    async def start_operation(services, op_name, group, adv_id):
+        access = dict(access=[BaseWorld.Access.RED])
+        data = dict(name=op_name, group=group, adversary_id=adv_id,
+                    planner='', atomic_enabled=1, hidden=False)
+        await services.get('rest_svc').create_operation(access=access, data=data)
+
+    @staticmethod
+    async def is_operation_successful(services, op_name):
+        operation = (await services.get('data_svc').locate('operations', match=dict(name=op_name)))[0]
+        if len(operation.chain) == 1 and 'host.user.name' in [f.trait for f in operation.all_facts()]:
+            return True
+        return False
+
+    @staticmethod
+    async def cleanup_operation(services, op_name):
+        await services.get('rest_svc').delete_operation(data=dict(name=op_name))
