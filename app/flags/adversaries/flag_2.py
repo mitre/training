289cf1908355ca07bf300b5add4f0d721e4d88ba
ddavila54 <30359513+ddavila54@users.noreply.github.com>
2020-06-23 12:24:53 -0400

    Update flag_2.py

--- app/flags/adversaries/flag_2.py
+++ app/flags/adversaries/flag_2.py
@@ -1,7 +1,7 @@
 name = 'Create ability'
-challenge = 'Create a new ability from the UI named "My test ability". Put it under the discovery tactic and include ' \
-            'a payload and a cleanup command. Have the command print out the network interfaces and IP addresses on ' \
-            'the host. Hint: this would be "ifconfig" on a MacOS.'
+challenge = 'Create a new ability from the UI named "My test ability" and add it to the Certifiable adversary. Put ' \
+            'it under the discovery tactic and include a command to execute and a cleanup command. Have the command ' \
+            'print out the network interfaces and IP addresses on the host. Hint: this would be "ifconfig" on a MacOS.'
 extra_info = """Being able to create TTPs quickly and swap them in and out is a valuable skill. Adversaries use a
  TTP until it is no longer effective - and then they write new versions, make modifications or switch techniques
  entirely."""
