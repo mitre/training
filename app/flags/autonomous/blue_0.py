237200e445c6578776d564c2d1b47e8af4bc1b14
Govardhen Arunagiri <garunagiri@mitre.org>
2020-07-01 14:28:34 -0400

    fixing cert and one flag

--- app/flags/autonomous/blue_0.py
+++ app/flags/autonomous/blue_0.py
@@ -11,7 +11,6 @@ extra_info = """"""
 async def verify(services):
     for op in await services.get('data_svc').locate('operations',
                                                     match=dict(access=BaseWorld.Access.BLUE, name='Blue Autonomous')):
-        if op.adversary.adversary_id == '7e422753-ad7a-4401-bc8b-b12a28e69c25' and not op.atomic \
-                and op.planner.name == 'batch':
+        if op.adversary.adversary_id == '7e422753-ad7a-4401-bc8b-b12a28e69c25' and op.planner.name == 'batch':
             return True
     return False
