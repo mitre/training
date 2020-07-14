23d91a800cbaf65edcc4005269e76198c6e5fb7a
ddavila54 <30359513+ddavila54@users.noreply.github.com>
2020-07-14 16:50:28 -0400

    Update flag_5.py

--- app/flags/agents/flag_5.py
+++ app/flags/agents/flag_5.py
@@ -1,7 +1,9 @@
 from app.utility.base_world import BaseWorld
 
 name = 'Bootstrap abilities'
-challenge = 'Using a Stockpile ability, ensure new agents automatically run "whoami" on their first beacon. Locate the respective ability and enter the ability id.'
+challenge = """Using a Stockpile ability, ensure new agents automatically run "whoami" on their first beacon. Locate the ability 
+that executes this command and enter the ability id (Hint: Abilities and their id can be found in the `plugins/stockpile/data/abilities` 
+directory)."""
 extra_info = """Depending on the defensive tools installed on a compromised machine, an adversary may need to execute their mission
 quickly before getting detected and shut down. One common tactic is to give each new agent a set of instructions to
 run immediately after sending the first beacon in. These instructions may determine what software is installed on
