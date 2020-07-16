aeee1682ff63fe530bddfa2a084e9a64f6afd712
Govardhen Arunagiri <garunagiri@mitre.org>
2020-07-16 13:18:18 -0400

    Added text to flags

--- app/flags/hunt/blue_1.py
+++ app/flags/hunt/blue_1.py
@@ -2,8 +2,12 @@ from plugins.training.app.base_flag import BaseFlag
 
 
 name = 'Hunt Flag 1'
-challenge = ''
-extra_info = """"""
+challenge = 'Complete this flag by hunting for the CALDERA ability that will have been run within one minute of this' \
+            'flag activating. Once you find the Process ID of the process that ran the PowerShell command(s), enter' \
+            'the PID in the Hunt badge, matching the tactic and technique of the procedure that was run. The tactic' \
+            'is listed below. The specific technique is listed on the other side of this card as a hint.\n\n' \
+            'Tactic: Command and Control'
+extra_info = """Technique: Ingress Tool Transfer"""
 
 
 operation_name = 'training_hunt_1'
--- app/flags/hunt/blue_3.py
+++ app/flags/hunt/blue_3.py
@@ -2,8 +2,12 @@ from plugins.training.app.base_flag import BaseFlag
 
 
 name = 'Hunt Flag 3'
-challenge = ''
-extra_info = """"""
+challenge = 'Complete this flag by hunting for the CALDERA ability that will have been run within one minute of this' \
+            'flag activating. Once you find the Process ID of the process that ran the PowerShell command(s), enter' \
+            'the PID in the Hunt badge, matching the tactic and technique of the procedure that was run. The tactic' \
+            'is listed below. The specific technique is listed on the other side of this card as a hint.\n\n' \
+            'Tactic: Defense Evasion'
+extra_info = """Technique: Modify Registry"""
 
 
 operation_name = 'training_hunt_3'
