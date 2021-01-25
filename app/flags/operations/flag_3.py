9a7e2bd00171fb2f5aedfaeef62875ebb5c727cc
Fred <59877971+mrengstrom@users.noreply.github.com>
2021-01-25 09:31:57 -0600

    changing message for op/flag3 to let users know they need to end the operation (#109)

--- app/flags/operations/flag_3.py
+++ app/flags/operations/flag_3.py
@@ -2,7 +2,8 @@ name = 'Empty operation'
 challenge = 'Run a new operation, this time not selecting any profiles or groups. Add at least 5 potential links ' \
               'to the operation.'
 extra_info = """When you run an autonomous adversary emulation exercise, the operation can only run the tasks it is pre-configured
-to run. Using potential links, you can "toss in" any additional TTPs into a live, autonomous operation."""
+to run. Using potential links, you can "toss in" any additional TTPs into a live, autonomous operation. Make sure to end the operation
+for the flag status to update."""
 
 
 async def verify(services):
