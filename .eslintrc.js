7c69c35b1b24e3599b4e2ae92a6345d2e1774d1d
Bryan Worrell <bworrell@mitre.org>
2021-02-19 12:11:13 -0500

    Add basic eslint and configuration files.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jquery": true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "globals": {
+        stream: 'readonly',
+        restRequest: 'readonly',
+        certificates: 'readonly',
+    },
+    "rules": {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single', {'avoidEscape': true }]
+    }
+};
