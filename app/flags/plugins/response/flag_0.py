22ebe9e1d67245b531ea5d8352f4fde5af539f32
Bryan Worrell <bworrell@mitre.org>
2021-03-02 10:32:47 -0500

    Use parens for multi-line string literals.

--- app/flags/plugins/response/flag_0.py
+++ app/flags/plugins/response/flag_0.py
@@ -3,10 +3,16 @@ from plugins.training.app.c_flag import Flag
 
 class PluginsResponseFlag0(Flag):
     name = 'Blue agent'
-    challenge = 'Log in as a blue user and deploy a new blue agent. The agent should successfully beacon back to ' \
-                'this server instance. Blue agents should be run with elevated privileges.'
-    extra_info = """Most Endpoint Detection and Response products use agents that are installed on the endpoints that 
-    need to be protected. Sandcat can function as Caldera's Blue agent too."""
+
+    challenge = (
+        'Log in as a blue user and deploy a new blue agent. The agent should successfully beacon back to '
+        'this server instance. Blue agents should be run with elevated privileges.'
+    )
+
+    extra_info = (
+        'Most Endpoint Detection and Response products use agents that are installed on the endpoints that '
+        'need to be protected. Sandcat can function as Caldera\'s Blue agent too.'
+    )
 
     async def verify(self, services):
         agents = await services.get('data_svc').locate('agents')
