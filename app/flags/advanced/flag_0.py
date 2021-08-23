232abea11f877bac463f01fbde3b90269e063727
Daniel Kim <kimd@mitre.org>
2021-08-23 16:43:48 -0400

    Make update config module more relevant

--- app/flags/advanced/flag_0.py
+++ app/flags/advanced/flag_0.py
@@ -3,7 +3,7 @@ from plugins.training.app.c_flag import Flag
 
 class AdvancedFlag0(Flag):
     name = 'Update configs'
-    challenge = 'Update the app.contact.http configuration property to an externally-facing IPv4 address'
+    challenge = 'Update the app.contact.http configuration property to an externally-facing IPv4 address (not 0.0.0.0).'
     extra_info = (
         'C2 servers need to be made accessible to agents, either through agent-network-facing interfaces or proxies. '
         'Changing the app.contact.http configuration value sets the URL that agents will connect to with the HTTP  '
@@ -12,6 +12,4 @@ class AdvancedFlag0(Flag):
 
     async def verify(self, services):
         http_contact = services.get('app_svc').get_config('app.http.contact')
-        if http_contact != 'http://127.0.0.1:8888':
-            return True
-        return False
+        return not (http_contact.startswith('http://127.0.0.1') or http_contact.startswith('http://0.0.0.0'))
